780ab0da1ad28e1ff383908db13e6dcda4674a99
Andreea Miron <Andreea.Miron@rebeldot.com>
2023-03-28 13:59:36 +0300

    added dashboard comp

---
+++ packages/container/src/components/DashboardApp.js
@@ -0,0 +1,16 @@
+import React , {useRef, useEffect} from "react";
+import {mount} from 'dashboard/DashboardApp';
+
+
+export default () => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        if(ref){
+           mount(ref.current)  
+        }
+
+    },[]);
+
+    return <div ref={ref} />
+}
